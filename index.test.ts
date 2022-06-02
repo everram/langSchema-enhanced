edbe975a0f65216ea416eb00b8e7666afec4ab4e
everram <130886069+everram@users.noreply.github.com>
2022-06-02 21:41:12 +0000

    quick edit

--- index.test.ts
+++ index.test.ts
@@ -18,4 +18,4 @@ describe("zod tests", () => {
 })
 
 describe('booleans', () => {
-  it("should return true for a positive review", () => expect(sch
+  it("should return true for a positive review", () => expect(schema.bool("Did this review user 
