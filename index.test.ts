3e86e68f99bbea311c6bbcc8e262c51b685e2e9f
everram <130886069+everram@users.noreply.github.com>
2022-08-27 12:30:49 +0000

    v1.8.5

--- index.test.ts
+++ index.test.ts
@@ -28,4 +28,4 @@ describe('booleans', () => {
 describe('enums', () => {
   it("should classify basic values correctly", () => expect(schema.categorize("My favorite color is red", ["red", "blue", "green"])).resolves.toBe("red"))
 
-  it("sh
+  it("should not throw an error for an invali
