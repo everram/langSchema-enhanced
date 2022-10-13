30af363c2044f83cbd26d50dd9d56d0f9c50cb01
everram <130886069+everram@users.noreply.github.com>
2022-10-13 18:15:45 +0000

    Russian localization

--- index.test.ts
+++ index.test.ts
@@ -30,4 +30,6 @@ describe('enums', () => {
 
   it("should not throw an error for an invalid value", () => expect(schema.categorize("My favorite color is red", ["blue", "green"])).resolves.not.toThrow())
 
-  it("should classify values with spaces correctly", () => expect(schema.categorize("I prefer watching Science Fiction movies", ["Science Fiction", "Romantic Comedy", "Action Adventure"])).resolves.to
+  it("should classify values with spaces correctly", () => expect(schema.categorize("I prefer watching Science Fiction movies", ["Science Fiction", "Romantic Comedy", "Action Adventure"])).resolves.toBe("Science Fiction"))
+
+  it("shoul
