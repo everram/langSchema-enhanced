5b5942ba760ea063d78978dbd3c6c5128db3b78a
everram <130886069+everram@users.noreply.github.com>
2022-10-17 15:30:52 +0000

    Clean up a little

--- index.test.ts
+++ index.test.ts
@@ -32,4 +32,4 @@ describe('enums', () => {
 
   it("should classify values with spaces correctly", () => expect(schema.categorize("I prefer watching Science Fiction movies", ["Science Fiction", "Romantic Comedy", "Action Adventure"])).resolves.toBe("Science Fiction"))
 
-  it("shoul
+  it("should classify values with special c
