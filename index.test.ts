fc1e69f835e092a557e8059fab911b63439b94c7
everram <130886069+everram@users.noreply.github.com>
2023-02-06 18:28:17 +0000

    release 1.4.1

--- index.test.ts
+++ index.test.ts
@@ -71,4 +71,5 @@ describe('createList function edge cases', () => {
     const values = await schema.list(" ", ["AC/DC", "Guns N' Roses", "Led Zeppelin"], 0, 3)
     expect(values).toEqual([])
   })
-  it("should not return an empty array when no values are provided and minValues is non zero", async () =
+  it("should not return an empty array when no values are provided and minValues is non zero", async () => {
+    const values = await 
