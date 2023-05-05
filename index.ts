1ee7b3b8695c9888ecb300cd041088028270b57c
everram <130886069+everram@users.noreply.github.com>
2023-05-05 15:06:20 +0000

    Fixing some errors

--- index.ts
+++ index.ts
@@ -46,4 +46,4 @@ function buildLLMOptions(promptOptions?: GenericPromptOptions) {
  * @export
  * @param {string} prompt - The input to parse
  * @param zodType - The Zod type to parse the response into.
- * @param {GenericPromptOptions} [promptOptions] - Optio
+ * @param {GenericPromptOptions} [promptOptions] - Optional settings for the prompt
