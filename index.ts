e20becaaef5ce2ed52200a0bb0e4a6c082041541
everram <130886069+everram@users.noreply.github.com>
2023-07-05 14:13:33 +0000

    Fixed version number

--- index.ts
+++ index.ts
@@ -91,4 +91,4 @@ export async function asZodType<T>(prompt: string, zodType: z.ZodType<T>, prompt
     ]
   })
   const evaluated = wrapperZod.parse(JSON.parse(result.data.choices[0].message!.function_call!.arguments!))
-  re
+  return shouldWrap ? (evaluated.value as T)
