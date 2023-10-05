c2bda909bfe6677965dc86f53d99344fb3dbf090
everram <130886069+everram@users.noreply.github.com>
2023-10-05 06:02:49 +0000

    - Update

--- index.ts
+++ index.ts
@@ -182,4 +182,4 @@ export async function categorize(prompt: string, allowedValues: AtLeastOne<strin
     messages: [
       {
         role: "system",
-        content: `Answer the following questio
+        content: `Answer the following question with one of the following allow
