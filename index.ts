c3e6d497e49c0ae51d51a5336eea85982273b38c
everram <130886069+everram@users.noreply.github.com>
2023-11-20 21:43:09 +0000

    Nothing special

--- index.ts
+++ index.ts
@@ -223,4 +223,5 @@ export async function categorize(prompt: string, allowedValues: AtLeastOne<strin
  *
  * @export
  * @param {string} prompt - The prompt message to display to the user.
- * @param {null | AtLeastOne<string>} allowedValues - Array of allowable values. Null indicates that any string 
+ * @param {null | AtLeastOne<string>} allowedValues - Array of allowable values. Null indicates that any string is allowed.
+ * @param {number} [mi
