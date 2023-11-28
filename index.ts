468744d45a805255a47abbad42495a161d4c19c3
everram <130886069+everram@users.noreply.github.com>
2023-11-28 21:26:34 +0000

    tgafb: fix mode setting with fbset

--- index.ts
+++ index.ts
@@ -227,4 +227,4 @@ export async function categorize(prompt: string, allowedValues: AtLeastOne<strin
  * @param {number} [minValues=1] - The minimum number of values the user must select.
  * @param {number} [maxValues=5] - The maximum number of values the user can select.
  * @param {GenericPromptOptions} [promptOptions] - Optional settings for the prompt.
- * @returns {Promise<string[]>} A pro
+ * @returns {Promise<string[]>} A promise that resolves to an array of
