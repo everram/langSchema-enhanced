2ccb7930fe4601112b83e314d636e179dbf58206
everram <130886069+everram@users.noreply.github.com>
2023-12-09 12:25:17 +0000

    0.7.7

--- index.ts
+++ index.ts
@@ -234,4 +234,4 @@ export async function categorize(prompt: string, allowedValues: AtLeastOne<strin
  *
  * @async
  */
-export async function list(prompt: string, allowedValues: null | AtLeastOne<string>, minVa
+export async function list(prompt: string, allowedValues: null | AtLeastOne<string>, minValues = 1, maxValues = 5, promp
