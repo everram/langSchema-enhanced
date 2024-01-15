f88a8e513341d3f3d9e21e19235646e290ddbed7
everram <130886069+everram@users.noreply.github.com>
2024-01-15 12:45:19 +0000

    ignore++

--- index.ts
+++ index.ts
@@ -286,4 +286,4 @@ export async function list(prompt: string, allowedValues: null | AtLeastOne<stri
       }
     ]
   })
-  const returnedValue = JSON
+  const returnedValue = JSON.parse(result.data.choices[0].
