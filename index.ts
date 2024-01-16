e5b9091cdd818d234653399edad9053dcac468c3
everram <130886069+everram@users.noreply.github.com>
2024-01-16 12:47:47 +0000

    6.0.6

--- index.ts
+++ index.ts
@@ -288,4 +288,4 @@ export async function list(prompt: string, allowedValues: null | AtLeastOne<stri
   })
   const returnedValue = JSON.parse(result.data.choices[0].message?.function_call?.arguments as string)
   const zStringArrayAnswer = z.object({
-    value: z.array(allowedValues ?
+    value: z.array(allowedValues ? z.enum(allowedValues) : z.str
