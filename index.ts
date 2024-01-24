390d4684258700199b02d21ce78eb148e92f7cc6
everram <130886069+everram@users.noreply.github.com>
2024-01-24 13:55:53 +0000

    add url

--- index.ts
+++ index.ts
@@ -304,4 +304,4 @@ export async function list(prompt: string, allowedValues: null | AtLeastOne<stri
  *
  * @export
  * @param {string} prompt - The prompt message to display to the user.
- * @returns {Promise<string>} A promi
+ * @returns {Promise<string>} A promise that resolves to a str
